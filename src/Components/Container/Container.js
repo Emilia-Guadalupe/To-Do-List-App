f574c5afbb3d1e75948fe93572c37b80bc908aac
Emilia <m.emilia.guadalupe@gmail.com>
2021-04-08 15:22:51 -0300

    Components of the list

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import FormToDo from '../FormToDo/FormToDo';
+import TaskList from '../TaskList/TaskList';
 
 function Container(){
+
+    const [list, setList] = useState([]); 
+
+    const handleAddItem = (addItem) => {
+        setList([...list, addItem]);
+    };
+
     return(
         <div>
-            Container!
+            <FormToDo handleAddItem={handleAddItem} />
+            <TaskList list={list} setList={setList} />
         </div>
     )
 }
